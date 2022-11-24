15a4f88f69f81260b09e262bc9c791df39828956
Leandro <leandroj.r.ximenes@gmail.com>
2022-11-24 18:11:56 +0000

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request
+from helpers import login_required
+from flask_session import Session
+import uuid
 
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+USERS = []
+
 @app.route("/")
-def hello_world():
+@login_required
+def index():
     return render_template("index.html", title="Hello")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        userid = str(uuid.uuid4())
+        # userid = str(uuid.uuid4().hex)
+        room = str(uuid.uuid4())
+        session["user_id"] = userid
+        session["room"] = room
+        USERS.append({
+            'id_user': userid,
+            'name': request.form.get("name"),
+            'room': room
+        })
+        return redirect("/")
+    else:
+        return render_template("login.html")
