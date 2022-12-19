3c284e507f474b4a986a7e09bbaf73a6ba83ef02
Leandro <leandroj.r.ximenes@gmail.com>
2022-12-19 15:08:50 +0000

    ran prod

--- assets/src/index.js
+++ assets/src/index.js
@@ -2,7 +2,6 @@ import { getURL, getParameters } from './components/util.js';
 
 const { userId, userName, userEmail } = getParameters()
 window.URL = getURL()
-console.log(window.URL)
 window.userId = userId
 window.userName = userName
 window.userEmail = userEmail
