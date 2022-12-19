9937972e38e78aca93a8a548d0f7b5a5985d5fa3
Leandro <leandroj.r.ximenes@gmail.com>
2022-12-19 14:58:35 +0000

    changing wss to ws

--- assets/src/components/util.js
+++ assets/src/components/util.js
@@ -1,6 +1,6 @@
 const getURL = () => {
     if (window.location.host === "memory-in-pairs.herokuapp.com") {
-        return "wss://memory-in-pairs-ws.herokuapp.com/";
+        return "ws://memory-in-pairs-ws.herokuapp.com/";
     } else if (window.location.host === "localhost:5000") {
         return "ws://localhost:5678/";
     } else {
