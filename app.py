68934ec7ff2d8821fefaf286365d05fc382fb0f0
Leandro <leandroj.r.ximenes@gmail.com>
2022-12-20 14:34:01 +0000

    add requirements

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import secrets
 from flask import Flask, render_template, redirect, session, request
-from helpers import login_required, getRandomTitle, getImagesList
+from helpers import login_required, getRandomTitle, getImagesList, getStringDB
 from flask_session import Session
 from cs50 import SQL
 from datetime import datetime, timezone
@@ -14,7 +14,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("postgresql://gszsssrnqlemqf:58f8e27182d1380dfb561540081d4cf651efb1a1a93ca830d55adb1d6a415621@ec2-34-231-63-30.compute-1.amazonaws.com:5432/dfv11i60s07f32")
+db = SQL(getStringDB())
 
 @app.route("/")
 @login_required
